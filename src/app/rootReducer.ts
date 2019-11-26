ce3d487d91c2d44a34031f03b09d2fe53fafee0b
ihorjs <ihorshevtsov137@gmail.com>
2019-11-26 14:22:30 -0800

    Add Redux Toolkit & React-Redux packages
    and add store and root reducer

---
+++ src/app/rootReducer.ts
@@ -0,0 +1,9 @@
+import { combineReducers } from "@reduxjs/toolkit";
+
+const rootReducer = combineReducers({
+  // reducers
+});
+
+export type RootState = ReturnType<typeof rootReducer>;
+
+export default rootReducer;
